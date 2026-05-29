2914fc07b6f28cbcb860235b1da078f38773c8ff
dma40 <dma99878@gmail.com>
2026-05-29 12:11:50 -0400

    fix a bug in the quality function
    this should produce the correct iteration as outlined by the Bellman equations

--- quality_function/qfunc.py
+++ quality_function/qfunc.py
@@ -1,12 +1,20 @@
-def q_func(state_action_transitions: dict[int, dict[int, tuple[int, int]]], 
-           time: int, discount: float) -> list[dict[int, dict[int, float]]]:
+def q_func(
+    state_action_transitions: dict[int, dict[int, tuple[int, int]]], 
+    time: int,
+    discount: float
+)-> list[dict[int, dict[int, float]]]:
     
-    quality_func = [0] * (time + 1)
+    quality_func = [] 
 
-    for i in range(time):
-        quality_func[i] = dict[int, dict[int, float]]
+    for _ in range(time + 1):
+        timestep = {}
 
-    # set everything in the (t + 1)th slot to 0
+        for state in state_action_transitions:
+                timestep[state] = {action: 0.0 for action in state_action_transitions[state]}
+        quality_func.append(timestep)
+
+    # Here, we will set everything in the (t + 1)th timeslot to zero, as the simulation
+    # will end at that point. 
 
     for state in state_action_transitions:
         for action in state_action_transitions[state]:
@@ -14,25 +22,20 @@ def q_func(state_action_transitions: dict[int, dict[int, tuple[int, int]]],
                 quality_func[time][state][action][resulting_state] = 0                
 
     for i in range(1, time + 1):
-        # populate the table using a DP approach
-        index = time + 1 - i
+        # populate the table using a backwards DP approach 
+        index = time - i
 
         for state in state_action_transitions:
-            for action in state_action_transitions[state]:
-                transition_table = state_action_transitions[state][action]
-                ev = 0
+            for action, transition_table in state_action_transitions[state].items():
+                ev = 0.0
 
-                for transition in transition_table:
-                    probability = transition_table[transition][0]
+                for next_state, (probability, reward) in transition_table.items():
+                    next_q = quality_func[index + 1].get(next_state, {}) # Bellman equation 
 
-                    previous = quality_func[index - 1][state][action]
-                    max_value = max(previous.values())
+                    max_future = max(next_q.values()) if next_q else 0.0
 
-                    ev += probability * (transition_table[transition][1] + discount * max_value)
+                    ev += probability * (reward + discount * max_future)
 
                 quality_func[index][state][action] = ev
 
     return quality_func
-       
-                            
-
